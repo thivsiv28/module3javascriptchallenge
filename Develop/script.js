493f3a88405e49a716c6775df6fe12ba02fc65d0
TSIV28 <thiviya.sivasithamparam@gmail.com>
2022-09-16 23:01:47 -0400

    finalreadmeadditions

--- Develop/script.js
+++ Develop/script.js
@@ -8,16 +8,19 @@ const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k", "l",
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-// var specialCharacters = ["!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-
 const specialCharacters = ["$", "%", "&", "*", "#", ">"];
 
 
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", promptPasswordLength);
+
+
+// function for getting length prompt
 function promptPasswordLength () {
 
-  const passwordLength = prompt("How many characters would you like your password to be?");
+  const passwordLength = prompt("How many characters would you like your password to be, please pick a number from 8 - 128 characters?");
   if (passwordLength < 8) {
     alert("Password length must be 8 characters minimum.");
     promptPasswordLength ();
@@ -30,32 +33,32 @@ function promptPasswordLength () {
 
  else if (isNaN(passwordLength) == true) {
 
-  alert ("Enter a valid integer");
+  alert ("Enter a valid number please!");
   promptPasswordLength ();
+
  }
 
   else { 
     promptUpperCase ({passwordLength});
   }
-
-  
 }
 
+// function for uppercase letters prompt
 function promptUpperCase ({passwordLength}) {
-  const wantUpperCase = prompt("Would you like Upper case letters? (yes/no)");
+  const wantUpperCase = prompt("Would you like upper case letters? (yes/no?)");
   let isUpperCase = false;
   if (wantUpperCase == "yes") {
     isUpperCase=true;
-
   }
   
   promptLowerCase({passwordLength, isUpperCase});
 
 }
 
+// function for lowercase letters prompt
 function promptLowerCase ({passwordLength, isUpperCase})
  {
-  const wantLowerCase = prompt("Would you like lower caser letters? (yes/no)");
+  const wantLowerCase = prompt("Would you like lower caser letters? (yes/no?)");
 
   let isLowerCase = false;
   if (wantLowerCase == "yes") {
@@ -63,11 +66,9 @@ function promptLowerCase ({passwordLength, isUpperCase})
   }
 
   promptNumbers({passwordLength, isLowerCase, isUpperCase})
-
-console.log("user selected lower case", isLowerCase, passwordLength, isUpperCase);
- 
 }
 
+//function for numbers prompt
 function promptNumbers ({passwordLength, isLowerCase, isUpperCase})
 {
   const wantNumbers = prompt("Would you like numbers? (yes/no?)");
@@ -79,6 +80,7 @@ function promptNumbers ({passwordLength, isLowerCase, isUpperCase})
   promptSpecialCharacters({passwordLength, isLowerCase, isNumbers, isUpperCase})
 }
 
+//function for specialcharacters prompt
 function promptSpecialCharacters ({passwordLength, isLowerCase, isNumbers, isUpperCase,})
 {
   const wantSpecialCharacters = prompt("Would you like special characters? (yes/no?)");
@@ -101,11 +103,11 @@ function writePassword({passwordLength, isLowerCase, isNumbers, isUpperCase, isS
   var password = generatePassword({passwordLength, isLowerCase, isNumbers, isUpperCase, isSpecialCharacters});
   var passwordText = document.querySelector("#password");
   
-
   passwordText.value = password;
 
 }
 
+//determining based off the users choice and how many cateogories of characters they picked, the final password
 function generatePassword({passwordLength, isLowerCase, isNumbers, isUpperCase, isSpecialCharacters})
 {
   let chunks = 0;
@@ -121,33 +123,28 @@ function generatePassword({passwordLength, isLowerCase, isNumbers, isUpperCase, 
 
   if(isNumbers == true) {
     numberPass = generateSimplePass (passwordLength,numbers);
-
     chunks= chunks+1;
   }
 
   if(isUpperCase == true) {
     upperCasePass = generateSimplePass (passwordLength,upperLetters);
-
     chunks= chunks+1;
   }
 
   if(isSpecialCharacters == true) {
     specialCharPass = generateSimplePass (passwordLength,specialCharacters);
-
     chunks= chunks+1;
   }
  
   if (chunks ==0) {
     alert ("You must enter yes for at lease one criteria");
-    return "click generate password to try again" ;
+    return "Click Generate Password button below to try again" ;
   }
 
   let chunkLength = passwordLength/chunks;
-
   let chunkRemainder= passwordLength%chunks;
-
   let randomPassword= "";
-debugger
+
   randomPassword=randomPassword + lowerCasePass.substring(0,chunkLength);
   randomPassword=randomPassword + upperCasePass.substring(0,chunkLength);
   randomPassword=randomPassword + numberPass.substring(0,chunkLength);
@@ -158,9 +155,7 @@ randomPassword=randomPassword + randomPassword.substring (0,chunkRemainder);
 
 }
 
-
-
-
+//determining how many characters length and letters there will be of each
 function generateSimplePass (passwordLength, candidateLetters) {
 
   let randomPassword = "";
@@ -173,13 +168,10 @@ function generateSimplePass (passwordLength, candidateLetters) {
   return randomPassword;
 }
 
-
-
 function showLengthSelector(){
   var lengthSelectorDiv = document.querySelector("#lengthinputcontainer");
 
   lengthSelectorDiv.style.display = "block";
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", promptPasswordLength);
+
 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # module3javascriptchallenge
 JavaScript Challenge: Password Generator
+
+This week's challenge was to modify the starter code to create an application that allows employees to generate random passwords based on the criteria  that they have selected. 
+
+## Some of the criteria that had to be met included:
+- User is presented with series of prompts for password criteria which included : choosing length of password (no less than 8 characters and no more than 128 characters), if they wanted lowercase or uppercase letters, and if they wanted numbers or special characters
+- User has to select at lease one character type
+- Password that is generated matched the selected criteria
+
+## Mock up:
+![The Password Generator application application.](./Assets/_C__Users_thivi_Desktop_challenge3_module3javascriptchallenge_Develop_index.html.png)
+
+## Deployed Link:
